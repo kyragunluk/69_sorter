2e9a9d4f910ec473fe9b48819279d6ae3448e01c
kyragunluk <kgunluk00@stuy.edu>
2019-05-06 14:03:53 -0400

    classwork

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -10,14 +10,28 @@ public class InsertionSorter extends Sorter {
       Construct an instance to process the user's data
      */
     public InsertionSorter(  ArrayList< String> usersData) {
-        // your code here, if necessary
+        for (int index = 0 ; index < usersData.size() ; index ++){
+            usersData = mySort(usersData,index);
+        }
     }
-    
+
 
     /**
       sort the user's data, implementing insertion sort
      */
-    public void mySort() {
-        // your code here
+    public void mySort(ArrayList< String> list, int alreadyInserted) {
+        String valueToAdd = list.get(alreadyInserted);
+        for (int index = alreadyInserted ; index > 0 ; index --){
+            if (valueToAdd.compareTo(list.get(index-1)) > 0s ){
+                for (int i = alreadyInserted ; i > index ; i --){
+                    list.set(i,list.get(i-1));
+                }
+                list.set(index,valueToAdd);
+            }
+        }
+        for (int i = alreadyInserted ; i > 0 ; i --){
+            list.set(i,list.get(i-1));
+        }
+        list.set(0,valueToAdd);
     }
 }
